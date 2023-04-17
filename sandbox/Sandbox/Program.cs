28e906f2e76afe1b1be4bd50fcbc071c78dbc5f2
Sariha Shepherd <sarihashepherd@Sarihas-MacBook-Air.local>
2023-04-16 19:59:53 -0700

    Completed Prep1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
+
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
     }
 }
